2d644ae1d47130853e63740b2d97a085d40e0704
Devika K S <devikaksuresh24@gmail.com>
2026-02-27 23:45:56 +0530

    add the file for code deploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,11 +2,7 @@
 set -e
 
 # Pull latest image
-docker pull devikaks24/simple-python-flask-app:latest
-
-# Stop old container if exists
-docker stop flask-app || true
-docker rm flask-app || true
+docker pull devikaks24/simple-python-flask-app
 
 # Run new container
-docker run -d -p 5000:5000 --name flask-app devikaks24/simple-python-flask-app:latest
+docker run -d -p 5000:5000 devikaks24/simple-python-flask-app
