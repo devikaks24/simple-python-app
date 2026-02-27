2b3b85744fdd114f57983917a069707f84cb3bf5
Devika K S <devikaksuresh24@gmail.com>
2026-02-27 23:23:57 +0530

    code deploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-docker pull devikaks24/simple-python-flask-app
+# Pull latest image
+docker pull devikaks24/simple-python-flask-app:latest
 
-# Run the Docker image as a container
-docker run -d -p devikaks24/simple-python-flask-app
+# Stop old container if exists
+docker stop flask-app || true
+docker rm flask-app || true
+
+# Run new container
+docker run -d -p 5000:5000 --name flask-app devikaks24/simple-python-flask-app:latest
